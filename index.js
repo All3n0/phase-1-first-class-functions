709d283ad7c83f096b014d8610bce8f207f9dec7
ALLAN <allankipr6@gmail.com>
2024-03-19 15:16:05 +0300

    kill

---
+++ index.js
@@ -0,0 +1,12 @@
+function receivesAFunction(callback){
+    callback()
+}
+function returnsAnAnonymousFunction(){
+    return function (){
+        
+    }
+
+}
+function returnsANamedFunction(){
+    return function jiku(){}
+}
